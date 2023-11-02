e5cd22036100e17ed2f94dcaa148d5bbd2579e69
aishasuleiman2002 <aishasuleimanali2002@gmail.com>
2023-11-02 15:51:49 +0300

    adding new changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div>
       <p className="text-right"> Welcome to Our  Blog Website</p>
+      <p className="text-right">sign up</p>
     <p className='text-center'>Blogs and News</p>
     </div>
   )
