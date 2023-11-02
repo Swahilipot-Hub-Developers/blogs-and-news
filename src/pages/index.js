3e30a33fd7141a245f66711694917a22f0ed2e7a
aishasuleiman2002 <aishasuleimanali2002@gmail.com>
2023-11-02 15:37:23 +0300

    adding new changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <p className='text-center'>Testing Bootstrap changes</p>
+      <p className='text-center'>Blogs and new Promise((resolve, reject) => {
+        
+      })</p>
     </div>
   )
 }
