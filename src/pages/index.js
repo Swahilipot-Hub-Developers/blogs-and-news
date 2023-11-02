cf842262f8f1cb734fde4dd6e32f9c8ad295e940
aishasuleiman2002 <aishasuleimanali2002@gmail.com>
2023-11-02 15:38:18 +0300

    adding new changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <p className='text-center'>Blogs and new Promise((resolve, reject) => {
-        
-      })</p>
+      <p className='text-center'>Blogs and News</p>
     </div>
   )
 }
