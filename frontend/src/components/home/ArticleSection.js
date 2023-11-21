8d47e19ee87136e8c827a4c7ac9264b42e3fe987
45jerryjoseph <45jerryjoseph@gmail.com>
2023-11-21 09:27:44 +0300

    Adding dependencies to the callback

--- frontend/src/components/home/ArticleSection.js
+++ frontend/src/components/home/ArticleSection.js
@@ -29,7 +29,7 @@ const ArticleSection = () => {
     } catch (error) {
       console.error('Error fetching articles:', error.message);
     }
-  },[]);
+  },[searchTerm,selectedCategory.id]);
   const fetchPublishedArticles = async () => {
     try {
       const response = await axios.get('http://127.0.0.1:8000/articles/published/');
